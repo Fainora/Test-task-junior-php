5333189c89feb506233b037ca38651ca0db39d0f
Fainora <fainora24@mail.ru>
2022-09-29 19:02:48 +0500

    fix
    + добавлены проверки и некоторые комментарии

--- public/task5.php
+++ public/task5.php
@@ -2,13 +2,15 @@
 
 function factorial($n)
 {
-    if($n === 1) {
+    if($n <= 1) {
         return 1;
     }
 
     return $n * factorial($n - 1);
 }
 
-echo factorial(1);
-echo "\n";
-echo factorial(5);
+echo factorial(1) . "\n"; // 1
+echo factorial(5) . "\n"; // 120
+echo factorial(3) . "\n"; // 6
+echo factorial(0) . "\n"; // 1
+echo factorial(-1) . "\n";// 1
--- public/task6.php
+++ public/task6.php
@@ -1,7 +1,8 @@
 <?php
-
+// Находим дубликаты и их кол-во
 $sql = "SELECT `name`,
     COUNT(`name`) AS `count`
     FROM `clients`
     GROUP BY `name`
     HAVING `count` > 1";
+    
